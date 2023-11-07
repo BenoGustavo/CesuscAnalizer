507056bac636c8bde69645c7ff80cec498f8dd5b
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-07 11:59:33 -0300

    remove most of the code and leave it to just run the app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 from PySide6.QtWidgets import QApplication
-from screens.setupUIs import RegisterUserWindow, SelectUserWindow
+from screens.controllers.selectUserController import SelectUserWindow
 from database.connection.studantController import studantsController
 
 if __name__ == "__main__":
     database = studantsController()
     app = QApplication()
-    window = RegisterUserWindow()
-    window2 = SelectUserWindow()
+    window = SelectUserWindow()
     app.exec()
