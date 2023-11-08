70c3aaab5469a75c93b5caca88a127ab8ee32016
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-08 17:54:49 -0300

    chrome binaries and make browser module

---
+++ src/scraping/browserInstance/makeChromeInstance.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+
+from sys import platform
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+UNIX_SYSTEMS = ["linux", "linux2", "darwin"]
+# if the platform is a unix based system
+if platform in UNIX_SYSTEMS:
+    CHROME_DRIVER_PATH = ROOT_PATH / "bin" / "chromedriver"
+# else is windows
+else:
+    CHROME_DRIVER_PATH = ROOT_PATH / "bin" / "chromedriver.exe"
+
+
+def makeBrowser(*options: str) -> webdriver.Chrome:
+    # check the version of the chromedriver
+    chromedriver_autoinstaller.install(path=CHROME_DRIVER_PATH.parent)
+
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    browser.set_window_size(1200, 700)
+
+    return browser
