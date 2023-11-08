c362ee1089b35938cf77031f01955019bb66a257
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-08 12:36:35 -0300

    added comments

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication
 from screens.controllers.selectUserController import SelectUserWindow
 from database.connection.studantController import studantsController
 
+# Creates the app
 if __name__ == "__main__":
     database = studantsController()
     app = QApplication()
