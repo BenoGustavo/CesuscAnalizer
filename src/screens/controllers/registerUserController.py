189b98f3f2670f735f11bf19f57b93c995e2b434
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-08 12:38:15 -0300

    added comments and now cleans the inputs after creating a user

--- src/screens/controllers/registerUserController.py
+++ src/screens/controllers/registerUserController.py
@@ -3,6 +3,9 @@ from screens.registerScreen import Ui_RegisterUserWindow
 from database.connection.studantController import studantsController
 from database.studantModel import StudantModel
 
+"""This module is responsible for the register user window
+All kinds of verification and database manipulation is done here"""
+
 
 class RegisterUserWindow(QMainWindow, Ui_RegisterUserWindow):
     def __init__(self, parent=None):
@@ -10,23 +13,34 @@ class RegisterUserWindow(QMainWindow, Ui_RegisterUserWindow):
         self.setupUi(self)
         ############################
 
-        self.configButtons()
+        self.__configButtons()
 
         ############################
 
         self.show()
 
-    def configButtons(self):
+    def __configButtons(self):
+        """Configure all the buttons in the screen with their respective functions"""
+
         self.returnButton.clicked.connect(self.returnButtonClicked)
         self.registerButton.clicked.connect(self.registerButtonClicked)
 
     def registerButtonClicked(self):
+        """This method is triggered by the register button, it creates a new user in the database
+
+        First it checks if all the fields are filled, if not it shows an error message
+
+        Then it creates a new user in the database and shows a message saying that the user was created
+
+        Then it clears all the fields"""
+
         # Check if all fields are filled
         if (
             self.usernameInput.isModified() == False
             or self.matriculaInput.isModified() == False
             or self.passwordInput.isModified() == False
         ):
+            # show the popup if needed
             self.__showErrorMessage(
                 "Preencha todos os campos",
                 QMessageBox.Icon.Warning,
@@ -34,22 +48,35 @@ class RegisterUserWindow(QMainWindow, Ui_RegisterUserWindow):
             )
             return
 
+        # get the text from the inputs
         username = self.usernameInput.text()
         enrollment_number = self.matriculaInput.text()
         password = self.passwordInput.text()
 
+        # create a new user from the studant model and then create it in the database
         newStudant = StudantModel(username, enrollment_number, password)
         studantsController().create(newStudant)
 
+        # show the popup saying that the user was created
         self.__showErrorMessage(
             "Usuário registrado com sucesso!",
             QMessageBox.Icon.Information,
             "Usuário registrado",
         )
 
+        # clear all the fields
+        self.usernameInput.clear()
+        self.matriculaInput.clear()
+        self.passwordInput.clear()
+
     def returnButtonClicked(self):
+        """This method is triggered by the return button, it hides the current window and shows the select user window"""
         self.hide()
-        from screens.controllers.selectUserController import SelectUserWindow
+
+        # import the select user window here to avoid circular imports########
+        from screens.controllers.selectUserController import SelectUserWindow  #
+
+        ##################################################
 
         self.selectUserWindow = SelectUserWindow()
         self.selectUserWindow.show()
