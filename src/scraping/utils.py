74587735eb9105f1141d897dd7093cccc26913fa
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-11-16 12:44:41 -0300

    fix:method that create dirs now create recursively, add: lambda functions to mount the path to excel and reports dir

--- src/scraping/utils.py
+++ src/scraping/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from os import path as systemPath
-from os import mkdir
-import datetime
+from os import makedirs
+from datetime import datetime
 
 
 def getCurrentTime() -> str:
@@ -19,7 +19,7 @@ def isFileCreated(filePath) -> bool:
 
 
 def createReportsDir(filePath) -> None:
-    mkdir(filePath)
+    makedirs(filePath)
 
 
 URL = "https://graduacao.cesusc.edu.br/projetos/nucleo/uteis/login.php?&tid=0&lid=0&pid=24&sid=98f8ec09184&&arq_ret=R5QT1WSRQBMCVQVPFFQSF99MCT5RT44Q9WRW0RBM0FMM5QQ4R4CV59RWRF1F5SWCW0"
@@ -41,17 +41,14 @@ XLSX_HEADERS = [
     "Frequencia em %",
 ]
 
-ROOT_PATH = Path(__file__).parent
+ROOT_PATH = Path(__file__).parent.parent.parent
 
 CREDENCIALS_PATH = ROOT_PATH / ".credentials.json"
 
-REPORTS_PATH = ROOT_PATH / "out" / "reports"
+getReportsPath = lambda username: ROOT_PATH / "out" / "reports" / username
 
 
-def getExcelPath(username: str):
-    EXCEL_PATH = REPORTS_PATH / username / "Cesusc_Info.xlsx"
-
-    return EXCEL_PATH
+getExcelPath = lambda REPORTS_PATH: REPORTS_PATH / "Cesusc_Info.xlsx"
 
 
 WAITING_TIME = 5
